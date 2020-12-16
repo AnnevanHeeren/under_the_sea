bc19692bee2d4dccb9ca94795c8f33902033e7ec
AnnevanHeeren <70519503+AnnevanHeeren@users.noreply.github.com>
2020-12-16 11:34:27 +0100

    Update app.js

---
+++ Game/code/build/app.js
@@ -0,0 +1,21 @@
+class CurrentScreen {
+    constructor() {
+    }
+}
+class Game {
+    constructor() {
+    }
+}
+class Obstacle {
+    constructor() {
+    }
+}
+class level {
+    constructor() {
+    }
+}
+class Player {
+    constructor() {
+    }
+}
+//# sourceMappingURL=app.js.map
