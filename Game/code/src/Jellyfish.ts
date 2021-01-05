6f0ab3719111b335ce21e528562d0d5f93165a63
AnnevanHeeren <70519503+AnnevanHeeren@users.noreply.github.com>
2021-01-05 09:12:18 +0100

    Update Jellyfish.ts

--- Game/code/src/Jellyfish.ts
+++ Game/code/src/Jellyfish.ts
@@ -1,7 +1,11 @@
 /// <reference path="Obstacle.ts" />
 
-class Jellyfish extends Obstacle{
-    public constructor() {
-        super();
+class Jellyfish extends Obstacle {
+
+    public constructor(canvas: HTMLCanvasElement) {
+        super(canvas);
+        this.image = this.loadNewImage("assets/images/jellyfish.png");
+        this.points = 5;
+        //this.question = true;
     }
 }
