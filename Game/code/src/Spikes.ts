1bd4c32b5f50aea7e6709104028b411111aaf6a6
AnnevanHeeren <70519503+AnnevanHeeren@users.noreply.github.com>
2021-01-05 09:12:33 +0100

    Update Spikes.ts

--- Game/code/src/Spikes.ts
+++ Game/code/src/Spikes.ts
@@ -1,7 +1,11 @@
 /// <reference path="Obstacle.ts" />
 
-class Spikes extends Obstacle{
-    public constructor() {
-        super();
+class Spikes extends Obstacle {
+
+    public constructor(canvas: HTMLCanvasElement) {
+        super(canvas);
+        this.image = this.loadNewImage("assets/images/spikes.png");
+        this.points = -2;
+        //this.question = false;
     }
 }
