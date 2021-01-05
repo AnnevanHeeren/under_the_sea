bac154372ffe7b2aa0b9b694c0ee08d43f1251c7
AnnevanHeeren <70519503+AnnevanHeeren@users.noreply.github.com>
2021-01-05 09:12:31 +0100

    Update Shark.ts

--- Game/code/src/Shark.ts
+++ Game/code/src/Shark.ts
@@ -1,7 +1,11 @@
 /// <reference path="Obstacle.ts" />
 
-class Shark extends Obstacle{
-    public constructor() {
-        super();
+class Shark extends Obstacle {
+
+    public constructor(canvas: HTMLCanvasElement) {
+        super(canvas);
+        this.image = this.loadNewImage("assets/images/shark.png");
+        this.points = 0;
+        //this.question = true;
     }
 }
