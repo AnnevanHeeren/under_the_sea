2a75619110a5658e1553331c3b5ddaa35fb24739
AnnevanHeeren <70519503+AnnevanHeeren@users.noreply.github.com>
2021-01-11 10:51:53 +0100

    Update Fish.ts

--- Game/code/src/Fish.ts
+++ Game/code/src/Fish.ts
@@ -4,7 +4,7 @@ class Fish extends Obstacle {
 
     public constructor(canvas: HTMLCanvasElement) {
         super(canvas);
-        this.image = this.loadNewImage("assets/images/resizedFish.png");
+        this.image = this.loadNewImage("assets/images/flippedresizedFish.png");
         this.points = 10;
         //this.question = false;
     }
